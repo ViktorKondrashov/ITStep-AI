392609e702255281dd2448514cc28784a51006ea
ViktorKondrashov <Pogoda123@&>
2026-01-15 23:28:36 +0200

    commit Homework16

---
+++ Homework16.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import (
+    HumanMessage,
+    AIMessage,
+    SystemMessage,
+)
+
+# заголовок
+st.title("Translator chat bot")
+
+api_key = st.secrets.get("GEMINI_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model='gemini-2.5-flash-lite',
+    api_key=api_key,
+)
+
+user_query = st.chat_input("Ваше повідомлення")
+
+if user_query is None:
+    # історія повідомлень
+    st.session_state['history'] = [
+        SystemMessage(
+            """
+            Ти -- ввічливий чат бот, твоя задача допомогати з вивченням англійської мови.
+            Якщо користувач просить перекласти слово або фразу, виведи переклад та приклад використання у реченні.
+            Якщо користувач просить перекласти речення, виведи переклад та пояснення граматики, наприклад: структура there is/are, пасивна форма дієслова, тощо.
+            """
+        )
+    ]
+
+if user_query:
+    human_message = HumanMessage(user_query)
+
+    st.session_state['history'].append(human_message)
+
+    response = llm.invoke(st.session_state['history'])
+
+    st.session_state['history'].append(response)
+
+for message in st.session_state['history']:
+    if isinstance(message, SystemMessage):
+        continue
+
+    text = message.content
+
+    if isinstance(message, HumanMessage):
+        role = "human"
+    else:
+        role = 'ai'
+
+    with st.chat_message(role):
+        st.markdown(text)
---
+++ data/lesson1/practice1.py
@@ -0,0 +1,21 @@
+# import torch
+# from torch.optim import SGD
+#
+# x = torch.tensor(3.5, requires_grad=True)
+#
+# optimizer = SGD(params=[x], lr=0.01)
+#
+# for epoch in range(100):
+#
+#     nn_result = f(x)
+#
+#     error = y_true - nn_result
+#
+#     error.backward()
+#
+#     optimizer.step()
+#
+#     optimizer.zero_grad()
+#
+#     print(epoch, error)
+
